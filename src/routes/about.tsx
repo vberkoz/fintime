ea7101f6cd014db12268f7dfb285f76cb4c86d91
Vasyl Berkoz <vberkoz@gmail.com>
2025-02-22 21:05:15 +0200

    first commit

---
+++ src/routes/about.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/about')({
+  component: About,
+})
+
+function About() {
+  return <div className="p-2 text-amber-700">Hello from About!</div>
+}
