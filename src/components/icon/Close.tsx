d90c6683f870d39ccf9663c6c0f79e33c48bee80
Vasyl Berkoz <vberkoz@gmail.com>
2025-02-23 23:02:00 +0200

    add icons

---
+++ src/components/icon/Close.tsx
@@ -0,0 +1,7 @@
+const Close = () => {
+  return (
+    <svg focusable="false" preserveAspectRatio="xMidYMid meet" fill="currentColor" width="32" height="32" viewBox="0 0 32 32" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"><path d="M17.4141 16L24 9.4141 22.5859 8 16 14.5859 9.4143 8 8 9.4141 14.5859 16 8 22.5859 9.4143 24 16 17.4141 22.5859 24 24 22.5859 17.4141 16z"></path><title>Close</title></svg>
+  );
+};
+
+export default Close;
---
+++ src/components/icon/OpenPanelLeft.tsx
@@ -0,0 +1,7 @@
+const OpenPanelLeft = () => {
+  return (
+    <svg focusable="false" preserveAspectRatio="xMidYMid meet" fill="currentColor" width="32" height="32" viewBox="0 0 32 32" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"><path d="M28,4H4A2,2,0,0,0,2,6V26a2,2,0,0,0,2,2H28a2,2,0,0,0,2-2V6A2,2,0,0,0,28,4ZM4,6h6V26H4ZM28,26H12V6H28Z"></path><title>Open panel left</title></svg>
+  );
+};
+
+export default OpenPanelLeft;
