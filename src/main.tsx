46c1b0a00ef7daf2a686c51f7d15df6fce87c747
Vasyl Berkoz <vberkoz@gmail.com>
2025-03-19 13:07:51 +0200

    minor refactoring

--- src/main.tsx
+++ src/main.tsx
@@ -33,6 +33,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-    </QueryClientProvider>,
-  </StrictMode>,
+    </QueryClientProvider>
+  </StrictMode>
 )
--- src/routes/time-tracking.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/time-tracking')({
-  component: TimeTracking,
-})
-
-function TimeTracking() {
-  return <div className="p-2">Time Tracking</div>
-}
