cd9ec7b3a453d1baaf78e48ef9da5dd8b0a881d6
Vasyl Berkoz <vberkoz@gmail.com>
2025-03-20 12:00:21 +0200

    add tanstack table

--- src/routes/income.tsx
+++ src/routes/income.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { IncomeList } from '../features/income/components/IncomeList'
 
 export const Route = createFileRoute('/income')({
   component: Income,
@@ -8,6 +9,7 @@ function Income() {
   return (
     <div className="p-4 space-y-4">
       <h3 className="text-3xl text-zinc-800 leading-8 mb-4">Income</h3>
+      <IncomeList />
     </div>
   )
 }
