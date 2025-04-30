2e7a99c0ff5a533e7777ca487d96f19bc5853924
Vasyl Berkoz <vberkoz@gmail.com>
2025-04-30 11:52:37 +0300

    update activity service

--- src/features/activity/services/activityService.ts
+++ src/features/activity/services/activityService.ts
@@ -1,17 +1,63 @@
 import type { Activity } from "../types";
 
-export const fetchActivities = async (): Promise<Activity[]> => {
-  const response = [
-    {
-      activityCategory: "development",
-      activityName: "Reviewed project requirements",
-      fundsDirection: "outbound",
-      fundsAmount: "0",
-      beginDate: "2023-10-01T09:00:00.000Z",
-      endDate: "2023-10-01T09:30:00.000Z",
-      activityNotes: "Discussed project scope with team.",
-    }
-  ]
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
 
-  return response
+export const fetchActivities = async (): Promise<Activity[]> => {
+  const url = `${apiBaseUrl}/api/activities/123/day/2025-04-26`; // Append sorting parameter
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch activities");
+  }
+  return response.json();
 }
+
+// Create a new activity
+export const createActivity = async (activity: Activity): Promise<Activity> => {
+  try {
+    const response = await fetch(`${apiBaseUrl}/api/activities`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userId: "123", endDate: activity.endDate, data: activity }),
+    });
+  
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.error || 'Failed to create driver');
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error('Error creating driver:');
+    throw error; // Re-throw the error for useMutation to handle
+  }
+};
+
+// Remove an activity
+export const removeActivity = async (endDate: string): Promise<Activity> => {
+  try {
+    const response = await fetch(`${apiBaseUrl}/api/activities/123/day/${endDate}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.error || 'Failed to remove driver');
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error('Error removing driver:');
+    throw error; // Re-throw the error for useMutation to handle
+  }
+};
