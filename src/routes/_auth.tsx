5ff1b7559344f67eeef99051b3a52ee314afb2fe
vberkoz <vberkoz@gmail.com>
2025-05-22 15:27:42 +0300

    refactor(auth): replace TopNavBar with AppSidebar and enhance UI
    Introduce AppSidebar for better navigation and improve the loading and error states. Replace the login prompt with an AlertDialog for a more polished user experience. This refactor aims to improve maintainability and user interaction.

--- src/routes/_auth.tsx
+++ src/routes/_auth.tsx
@@ -1,5 +1,20 @@
-import TopNavBar from "@/components/TopNavBar";
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { AppSidebar } from "@/components/AppSidebar";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import {
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { createFileRoute, Outlet, useLocation } from "@tanstack/react-router";
+import { Loader2 } from "lucide-react";
 import { useAuth } from "react-oidc-context";
 
 export const Route = createFileRoute("/_auth")({
@@ -8,25 +23,16 @@ export const Route = createFileRoute("/_auth")({
 
 function AuthLayout() {
     const auth = useAuth();
+    const location = useLocation();
 
     if (auth.isLoading) {
         return (
             <div className="flex items-center justify-center min-h-screen">
                 <div className="flex flex-col items-center justify-center">
-                    <span className="text-lg text-left font-semibold before:content-[''] after:animate-dots w-20">
-                        Loading
+                    <span className="flex text-lg text-left font-semibold items-center">
+                        Please wait
+                        <Loader2 className="ml-2 h-4 w-4 animate-spin" />
                     </span>
-                </div>
-            </div>
-        );
-    }
-
-    if (auth.error) {
-        return (
-            <div className="flex items-center justify-center min-h-screen">
-                <div className="flex flex-col items-center justify-center">
-                    <div>Encountering error</div>
-                    <div>{auth.error.message}</div>
                 </div>
             </div>
         );
@@ -34,26 +40,44 @@ function AuthLayout() {
 
     if (auth.isAuthenticated) {
         return (
-            <main className="p-2 overflow-y-scroll min-h-screen">
-                <TopNavBar />
-                <Outlet />
-            </main>
+            <SidebarProvider>
+                <AppSidebar />
+                <SidebarInset className="p-2">
+                    <div className="flex">
+                        <SidebarTrigger className="mb-2 mr-2" />
+                        {(() => {
+                            switch (location.href) {
+                                case "/monthly":
+                                    return <div className="pt-0.5">Monthly</div>;
+                                case "/categories":
+                                    return <div className="pt-0.5">Categories</div>;
+                                default:
+                                    return <div className="pt-0.5">Daily Activities</div>;
+                            }
+                        })()}
+                    </div>
+                    <Outlet />
+                </SidebarInset>
+            </SidebarProvider>
         );
     }
 
-    // If not authenticated, show login button and redirect
     return (
-        <div className="flex items-center justify-center min-h-screen">
-            <div className="flex flex-col items-center justify-center gap-4">
-                <h1 className="text-2xl font-bold">Welcome to FinTime</h1>
-                <p>Please log in to continue</p>
-                <button
-                    onClick={() => void auth.signinRedirect()}
-                    className="cursor-pointer flex items-center gap-2 px-4 py-2 rounded-md transition-colors bg-primary text-primary-foreground hover:bg-primary/90"
-                >
-                    Log In
-                </button>
-            </div>
-        </div>
+        <AlertDialog open={true}>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>You are logged out</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        You need to be logged in to view requested page. Please
+                        click login before continuing.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogAction onClick={() => auth.signinRedirect()}>
+                        Login
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
     );
 }
